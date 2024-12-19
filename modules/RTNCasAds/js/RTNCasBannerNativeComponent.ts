6e8c4199b6d415284cecc345196d147c9090d194
Vasili <grodno.by@mail.ru>
2024-12-19 13:36:37 +0300

    Move CasAds package to modules folder

--- modules/RTNCasAds/js/RTNCasBannerNativeComponent.ts
+++ modules/RTNCasAds/js/RTNCasBannerNativeComponent.ts
@@ -1,5 +1,5 @@
-import type { ViewProps } from "ViewPropTypes";
-import type { HostComponent } from "react-native";
+import type {ViewProps} from "ViewPropTypes";
+import type {HostComponent} from "react-native";
 import codegenNativeComponent from "react-native/Libraries/Utilities/codegenNativeComponent";
 
 export interface NativeProps extends ViewProps {
