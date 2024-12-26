0dcf86fbd3767adfa9ae8261f30690cf0107e730
Vasili <grodno.by@mail.ru>
2024-12-26 13:11:56 +0300

    Move specs to `scecs` folder

--- app/ads/BannerWrapper.tsx
+++ app/ads/BannerWrapper.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import RTNCasBanner from "rtn-cas-ads/js/RTNCasBannerNativeComponent";
+import RTNCasBanner from "rtn-cas-ads/js/specs/RTNCasBannerNativeComponent";
 
 enum BannerSize {BANNER, LEADERBOARD}
 
